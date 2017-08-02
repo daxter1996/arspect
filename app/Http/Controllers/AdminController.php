147e5e3700f0d44343159d29f8452c3bee4dab28
Jesus Liz Alles <jesuslizalles@gmail.com>
2017-08-02 09:39:44 +0100

    Perfil de Usuaris/Artistes

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    //
+
+    public function general(){
+        $totalArtistas = count(User::where('type', 2)->get());
+        return view('admin.general', compact('totalArtistas'));
+    }
+
+    public function validacion(){
+        $usersNoActive = User::where('active', 0)->get();
+        return view('admin.validacion', compact('usersNoActive'));
+    }
+}
